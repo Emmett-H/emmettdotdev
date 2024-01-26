a5eaa38a638f828f0c6fda7f2305628a1c583f40
Emmett-H <emmettharper101@hotmail.com>
2024-01-26 16:26:40 +0000

    add dark mode and toggle, default dark

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import "./App.css";
 
 function App() {
   const toggleTheme: MouseEventHandler<HTMLButtonElement> = () => {
-    document.body.classList.toggle("dark");
+    const isDark = document.documentElement.classList.toggle("dark");
+    localStorage.setItem('color-theme', isDark ? 'dark' : 'light');
   };
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">emmett.dev</h1>
-      <button onClick={toggleTheme}>Toggle Theme</button>
+      <h1 className="text-3xl font-bold underline text-primary">emmett.dev</h1>
+      <button className="text-primary" onClick={toggleTheme}>Toggle Theme</button>
     </>
   );
 }
