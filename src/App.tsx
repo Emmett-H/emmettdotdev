0715b981ba3ec7149196465d74837f60a4e29b37
Emmett-H <emmettharper101@hotmail.com>
2024-02-23 16:08:02 +0000

    Add shadcn UI to project + navigation menu

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,13 @@ function App() {
 
   return (
     <div className=" flex-col h-screen overflow-hidden flex justify-center items-center font-tektur">
-      <div className="typing-demo text-3xl text-primary font-tektur">emmett.dev</div>
+      <div className="typing-demo text-3xl text-primary font-tektur">
+        emmett.dev
+      </div>
       <button className="text-primary font-dm-sans" onClick={toggleTheme}>
         Toggle Theme
       </button>
     </div>
-
   );
 }
 
