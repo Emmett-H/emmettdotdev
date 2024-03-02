5e14fe62d3aff6e39802b235c7fed812ecd2af98
Emmett-H <emmettharper101@hotmail.com>
2024-03-02 18:56:00 +0000

    Fade in animation on logo fade out

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import "./App.css";
 
 function App() {
   const [showOverlay, setShowOverlay] = useState(true);
-  const [animationState, setAnimationState] = useState('typing');
+  const [animationState, setAnimationState] = useState("typing");
+  const [contentAnimation, setContentAnimation] = useState("");
 
   const toggleTheme = () => {
     const isDark = document.documentElement.classList.toggle("dark");
@@ -11,32 +12,56 @@ function App() {
   };
 
   useEffect(() => {
-    const currentTheme = localStorage.getItem("color-theme") || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+    const currentTheme =
+      localStorage.getItem("color-theme") ||
+      (window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light");
     document.documentElement.classList.toggle("dark", currentTheme === "dark");
 
     if (!localStorage.getItem("animation-played")) {
       setTimeout(() => {
-        setAnimationState('fadeOut');
+        setAnimationState("fadeOut");
         setTimeout(() => {
           setShowOverlay(false);
+          setContentAnimation("fade-in");
           localStorage.setItem("animation-played", "true");
         }, 1500);
       }, 4700);
     } else {
       setShowOverlay(false);
+      setContentAnimation("fade-in");
     }
   }, []);
 
   return (
     <>
       {showOverlay && (
-        <div className="absolute inset-0 z-50 flex justify-center items-center bg-background" style={animationState === 'fadeOut' ? { animation: 'fadeOut 1.5s forwards' } : {}}>
-          <div className={`text-3xl text-primary font-tektur ${animationState === 'typing' ? 'typing-demo' : ''}`}>
+        <div
+          className="absolute inset-0 z-50 flex justify-center items-center bg-background"
+          style={
+            animationState === "fadeOut"
+              ? { animation: "fadeOut 1.5s forwards" }
+              : {}
+          }
+        >
+          <div
+            className={`text-3xl text-primary font-tektur ${animationState === "typing" ? "typing-demo" : ""
+              }`}
+          >
             emmett.dev
           </div>
         </div>
       )}
-      <div className={`flex-col h-screen overflow-hidden flex justify-center items-center font-tektur ${showOverlay ? 'hidden' : ''}`}>
+      <div
+        className={`flex-col h-screen overflow-hidden flex justify-center items-center font-tektur ${showOverlay ? "hidden" : ""
+          } ${contentAnimation}`}
+        style={
+          contentAnimation === "fade-in"
+            ? { animation: "fadeIn 1.5s forwards" }
+            : {}
+        }
+      >
         <button className="text-primary font-dm-sans" onClick={toggleTheme}>
           Toggle Theme
         </button>
