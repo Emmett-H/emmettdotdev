e501e56e8e7fcea26e5887b76c7e8364be9d8ed3
Emmett-H <emmettharper101@hotmail.com>
2024-06-07 18:00:11 +0100

    Tidy experience section up

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import './App.css';
 import Header from './components/ui/header';
 import LinkedInIcon from './components/ui/linkedinIcon';
 import GitHubIcon from './components/ui/githubIcon';
-import OnTheBeachLogo from './components/ui/onthebeachLogo';
-import CinchLogo from './components/ui/cinchLogo';
-import ThalesLogo from './components/ui/thalesLogo';
+import ExperienceSection from './components/ui/experienceSection';
 import { TypeAnimation } from 'react-type-animation';
 
 function App() {
@@ -47,36 +45,6 @@ function App() {
       setContentAnimation('fade-in');
     }
   }, []);
-
-  const experiences = [
-    {
-      title: "Senior Software Engineer",
-      company: "cinch",
-      logo: CinchLogo,
-      logoWidth: 50,
-      logoHeight: 25,
-      period: "2021 - 2023",
-      link: "https://cinch.co.uk",
-    },
-    {
-      title: "UI Engineer",
-      company: "Thales",
-      logo: ThalesLogo,
-      logoWidth: 50,
-      logoHeight: 13,
-      period: "2021",
-      link: "https://thalesgroup.com",
-    },
-    {
-      title: "Software Engineer",
-      company: "Thales",
-      logo: ThalesLogo,
-      logoWidth: 50,
-      logoHeight: 13,
-      period: "2017 - 2021",
-      link: "https://thalesgroup.com",
-    },
-  ];
 
   return (
     <>
@@ -144,48 +112,7 @@ function App() {
           <h1 className="mb-4 mt-16 font-dm-sans text-lg font-bold">
             Experience
           </h1>
-          <ol className="relative border-s border-gray-200 font-dm-sans">
-            <li className="mb-5 ms-4">
-              <div className="absolute -start-1.5 mt-1.5 h-3 w-3 rounded-full bg-gray-500"></div>
-              <div className="flex flex-row items-center gap-2">
-                <div className="text-md font-medium">Engineering Lead</div>
-                <div className="inline-block rounded-full border px-1 py-0 text-xs uppercase">
-                  current
-                </div>
-              </div>
-              <a
-                href="https://onthebeach.co.uk"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="mb-4 inline-flex text-sm font-normal"
-              >
-                On the Beach
-                <span className="ml-2">
-                  <OnTheBeachLogo width={50} height={20} />
-                </span>
-              </a>
-            </li>
-            {experiences.map((exp, index) => (
-              <li className="mb-5 ms-4" key={index}>
-                <div className="absolute -start-1.5 mt-1.5 h-3 w-3 rounded-full bg-gray-300"></div>
-                <div className="text-md font-medium">
-                  {exp.title}
-                  <span className="ml-1 mt-1 text-xs">({exp.period})</span>
-                </div>
-                <a
-                  href={exp.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="inline-flex text-sm font-normal"
-                >
-                  {exp.company}
-                  <span className="ml-2 mt-0.5">
-                    <exp.logo width={exp.logoWidth} height={exp.logoHeight} />
-                  </span>
-                </a>
-              </li>
-            ))}
-          </ol>
+          <ExperienceSection />
         </div>
 
         <div
