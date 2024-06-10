ef8095ec497dd79b52f31037a59998b732c2c2b8
Emmett-H <emmettharper101@hotmail.com>
2024-06-10 20:29:52 +0100

    header position bug fix

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -6,7 +6,7 @@ interface HeaderProps {
 
 const Header = ({ toggleTheme }: HeaderProps) => {
     return (
-        <header className="sticky top-0 z-10 flex w-full h-14 items-center justify-between bg-background text-primary">
+        <header className="sticky top-0 z-10 flex w-full h-14 items-center justify-between bg-background text-primary -ml-1.5 sm:ml-0">
             <h1 className="text-center font-tektur text-3xl z-10">
                 <a href="/" className="block whitespace-nowrap">emmett.dev</a>
             </h1>
