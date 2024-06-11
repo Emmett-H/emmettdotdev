4693d571b53bb7baeec9e1645f3f73ed7f64c799
Emmett-H <emmettharper101@hotmail.com>
2024-06-11 21:37:43 +0100

    Wait that actually did work

--- src/components/ui/themeToggle.tsx
+++ src/components/ui/themeToggle.tsx
@@ -4,18 +4,36 @@ interface ToggleThemeProps {
     toggleTheme: () => void;
 }
 
-
 const ToggleTheme = ({ toggleTheme }: ToggleThemeProps) => {
     const [isChecked, setIsChecked] = useState(false);
 
     useEffect(() => {
-        const currentTheme = localStorage.getItem('color-theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+        const currentTheme =
+            localStorage.getItem('color-theme') ||
+            (window.matchMedia('(prefers-color-scheme: dark)').matches
+                ? 'dark'
+                : 'light');
         setIsChecked(currentTheme === 'dark');
         updateThemeColor(currentTheme);
     }, []);
 
+    useEffect(() => {
+        const updateThemeColorOnScroll = () => {
+            const currentTheme = isChecked ? 'dark' : 'light';
+            updateThemeColor(currentTheme);
+        };
+
+        window.addEventListener('scroll', updateThemeColorOnScroll);
+
+        return () => {
+            window.removeEventListener('scroll', updateThemeColorOnScroll);
+        };
+    }, [isChecked]);
+
     const updateThemeColor = (theme: string) => {
-        const themeColorMetaTag = document.querySelector('meta[name="theme-color"]');
+        const themeColorMetaTag = document.querySelector(
+            'meta[name="theme-color"]',
+        );
         if (themeColorMetaTag) {
             if (theme === 'dark') {
                 themeColorMetaTag.setAttribute('content', 'hsl(240, 3.7%, 15.9%)');
@@ -43,46 +61,40 @@ const ToggleTheme = ({ toggleTheme }: ToggleThemeProps) => {
             />
             <label
                 htmlFor="dn"
-                className={`toggle relative inline-block w-16 h-9 rounded-full cursor-pointer transition-colors duration-200 ease-cubic ${isChecked ? 'checked bg-slate-700' : 'bg-sky-200'
+                className={`toggle ease-cubic relative inline-block h-9 w-16 cursor-pointer rounded-full transition-colors duration-200 ${isChecked ? 'checked bg-slate-700' : 'bg-sky-200'
                     }`}
             >
                 <span
-                    className={`toggle__handler relative inline-block z-10 top-1 left-1 w-7 h-7 rounded-full shadow-md transition-all duration-400 ease-cubic ${isChecked
-                        ? 'bg-white transform translate-x-7 rotate-0'
-                        : 'bg-amber-200 transform rotate-[-45deg] sun'
+                    className={`toggle__handler duration-400 ease-cubic relative left-1 top-1 z-10 inline-block h-7 w-7 rounded-full shadow-md transition-all ${isChecked
+                            ? 'translate-x-7 rotate-0 transform bg-white'
+                            : 'sun rotate-[-45deg] transform bg-amber-200'
                         }`}
                 >
                     <span
-                        className={`absolute w-0.5 h-0.5 bg-slate-400 rounded-full transition-opacity duration-200 ease-in-out top-3 left-1.5 ${isChecked ? 'opacity-100' : 'opacity-0'
+                        className={`absolute left-1.5 top-3 h-0.5 w-0.5 rounded-full bg-slate-400 transition-opacity duration-200 ease-in-out ${isChecked ? 'opacity-100' : 'opacity-0'
                             }`}
                     ></span>
                     <span
-                        className={`absolute w-1 h-1 bg-slate-400 rounded-full transition-opacity duration-200 ease-in-out top-5 left-3 ${isChecked ? 'opacity-100' : 'opacity-0'
+                        className={`absolute left-3 top-5 h-1 w-1 rounded-full bg-slate-400 transition-opacity duration-200 ease-in-out ${isChecked ? 'opacity-100' : 'opacity-0'
                             }`}
                     ></span>
                     <span
-                        className={`absolute w-1.5 h-1.5 bg-slate-400 rounded-full transition-opacity duration-200 ease-in-out top-2 left-3.5 ${isChecked ? 'opacity-100' : 'opacity-0'
+                        className={`absolute left-3.5 top-2 h-1.5 w-1.5 rounded-full bg-slate-400 transition-opacity duration-200 ease-in-out ${isChecked ? 'opacity-100' : 'opacity-0'
                             }`}
                     ></span>
                 </span>
                 <span
-                    className={` absolute w-0.5 h-0.5 bg-white top-2 left-5 transition-all duration-300 ease-cubic ${!isChecked && 'w-6'}`}
+                    className={` ease-cubic absolute left-5 top-2 h-0.5 w-0.5 bg-white transition-all duration-300 ${!isChecked && 'w-6'}`}
+                ></span>
+                <span className="ease-cubic absolute left-3 top-6 h-0.5 w-0.5 bg-white transition-all duration-300"></span>
+                <span
+                    className={` ease-cubic absolute left-4 top-4 h-0.5 w-0.5 bg-white transition-all duration-300 ${!isChecked && 'top-5 w-6'}`}
                 ></span>
                 <span
-                    className='absolute w-0.5 h-0.5 bg-white top-6 left-3 transition-all duration-300 ease-cubic'
+                    className={` ease-cubic absolute left-2 top-3 z-50 h-0.5 w-0.5 bg-white transition-all duration-300 ${!isChecked && 'left-5 top-3.5 w-4'}`}
                 ></span>
-                <span
-                    className={` absolute w-0.5 h-0.5 bg-white top-4 left-4 transition-all duration-300 ease-cubic ${!isChecked && 'w-6 top-5'}`}
-                ></span>
-                <span
-                    className={` absolute w-0.5 h-0.5 bg-white top-3 left-2 z-50 transition-all duration-300 ease-cubic ${!isChecked && 'w-4 left-5 top-3.5'}`}
-                ></span>
-                <span
-                    className='absolute w-0.75 h-0.75 bg-white top-6 left-3 transition-all duration-300 ease-cubic'
-                ></span>
-                <span
-                    className='absolute w-0.5 h-0.5 bg-white top-7 left-5 transition-all duration-300 ease-cubic'
-                ></span>
+                <span className="w-0.75 h-0.75 ease-cubic absolute left-3 top-6 bg-white transition-all duration-300"></span>
+                <span className="ease-cubic absolute left-5 top-7 h-0.5 w-0.5 bg-white transition-all duration-300"></span>
             </label>
         </div>
     );
